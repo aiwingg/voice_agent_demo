8c239895bd88235b3df18a7a378152c1fe30c826
BigDan <akopian.da@phystech.edu>
2025-05-15 16:44:52 +0300

    make it better

--- .history/client/src/App_20250515164439.js
+++ .history/client/src/App_20250515164439.js
@@ -374,7 +374,7 @@ function App() {
 
       <div style={styles.cardContainer}>
         <div style={styles.header}>
-          <h1 style={styles.title}>{COMPANIES[companyId]['Company Name']}</h1>
+          <h1 style={styles.title}>{COMPANIES[companyId['Company Name']]}</h1>
           <p style={styles.subtitle}>{translations[language].subtitle}</p>
         </div>
       </div>
