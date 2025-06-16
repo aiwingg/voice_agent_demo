7bde266edfd84a37fe9a61c76c6bb1ed1a86f0db
BigDan <akopian.da@phystech.edu>
2025-06-16 11:05:59 +0300

    change defult

--- client/src/config.js
+++ client/src/config.js
@@ -2,7 +2,6 @@ export const DEFAULT_AGENT_ID = 'agent_d1b78ff3f73322fc34dd89abb5';
 export const DEFAULT_LANGUAGE = 'en';
 export const DEFAULT_COMPANY_NAME = 'Sycorax AI';
 
-// Export both the company mapping and default values
 export default {
   DEFAULT_AGENT_ID,
   DEFAULT_LANGUAGE,
